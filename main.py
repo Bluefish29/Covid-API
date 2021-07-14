b14540ca2a98058ad5cca98a9609d8fdde09195c
Bluefish29 <87444506+Bluefish29@users.noreply.github.com>
2021-07-14 15:55:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ window = tk.Tk()
 window.geometry("800x400")
 window.title("Covid Stats")
 
-for index in response.json()["Countries"]:
-    if index["Country"] == "Canada":
-        print(index)
+def show_data(country_name):
+
+    for index in response.json()["Countries"]:
+        if index["Country"] == country_name:
+            country_label = tk.Label(window, font = "Ariel 20",text = country_name)
+            country_label.pack()
+            total_confirmed = index["TotalConfirmed"]
+            confirmed_label = tk.Label(window, font = "Ariel 10", text = 'Total Confirmed Covid Cases:  ' + str(total_confirmed))
+            confirmed_label.pack()
+
+
 
 window.mainloop()
