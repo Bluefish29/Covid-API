8be7a8f116b9bd0c121899ce3fa022f92e39471b
Bluefish29 <87444506+Bluefish29@users.noreply.github.com>
2021-07-14 15:35:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import requests
+import tkinter as tk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+response = requests.get("http://api.covid19api.com/summary")
 
+window = tk.Tk()
+window.geometry("800x400")
+window.title("Covid Stats")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for index in response.json()["Countries"]:
+    if index["Country"] == "Canada":
+        print(index)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+window.mainloop()
