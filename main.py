e303458d60d5d314dff802f169a2766a115f6260
Bluefish29 <87444506+Bluefish29@users.noreply.github.com>
2021-07-14 16:03:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ window = tk.Tk()
 window.geometry("800x400")
 window.title("Covid Stats")
 
-def show_data(country_name):
-
+def show_data():
+    country_name = country_entry.get()
     for index in response.json()["Countries"]:
         if index["Country"] == country_name:
             country_label = tk.Label(window, font = "Ariel 20",text = country_name)
@@ -18,5 +18,8 @@ def show_data(country_name):
             confirmed_label.pack()
 
 
-
+country_entry = tk.Entry(window)
+country_entry.pack()
+search_button = tk.Button(window, text = "Search", command = show_data)
+search_button.pack()
 window.mainloop()
